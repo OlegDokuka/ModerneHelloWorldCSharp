0c40968b37830677cca7abf0ba595d65c72f892c
OlegDokuka <oleh@moderne.io>
2024-07-16 23:55:11 +0300

    Initial commit

---
+++ ModerneHelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ModerneHelloWorld;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");        
+    }
+}
